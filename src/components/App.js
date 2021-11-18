e0319082bbc102dfc7621efb7607dda698e121ef
Kepitss <keper524@gmail.com>
2021-11-18 15:36:06 +0200

    Added bootstrap and sass with basic styling
    - This application now uses for styling bootstrap and sass

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Container from "react-bootstrap/Container";
+
+import "../css/custom.css";
+
+const App = () => {
+	return (
+		<Container>
+			<h1>App component</h1>
+		</Container>
+	);
+};
+
+export default App;
